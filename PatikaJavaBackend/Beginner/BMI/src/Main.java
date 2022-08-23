10f453981fc6dd2ad4683e9c4e0235620ef61e76
gAtrium <pdimetil@gmail.com>
2022-08-23 12:24:25 +0000

    Add BMI calc, fix a logic error in circcirc.

--- PatikaJavaBackend/Beginner/BMI/src/Main.java
+++ PatikaJavaBackend/Beginner/BMI/src/Main.java
@@ -2,13 +2,13 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("Input the values in a.a,b.b format: ");
+        System.out.print("Input the values (weight(kg),height(m)) in a.a,b.b format: ");
         String inp = sc.nextLine();
         float[] vals = {0.0f,0.0f};
         String[] spl = inp.split(",+");
         for (int i = 0; i < 2; i++){
             vals[i] = Float.parseFloat(spl[i]);
         };
-        System.out.println(String.format("The area of your inputted section of a circle is: %f",(Math.PI*(vals[0]/2*vals[0]/2)  * vals[1]) /360));
+        System.out.println(String.format("Your BMI is: %f", vals[0] / (vals[1] * vals[1])));
     }
 }
